597004a4cee7ee88d029cfb5454990ec13550b94
Joakim Skjefstad <skjefstad.joakim@gmail.com>
2021-03-06 01:01:06 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
+// App.vue
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Health />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Health from './components/Health.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Health
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
